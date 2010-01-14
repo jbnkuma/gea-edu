6f3d13f6f10e950d6c84067f8eea182e8cadccf6
thelordofdragon <thelordofdragon@8f31eaa2-f0ef-11de-b2c3-d77de7315633>
2010-01-14 03:57:34 +0000

    Cabios script oracle

--- BD/Mysql/BD_mysql.sql
+++ BD/Mysql/BD_mysql.sql
@@ -2,6 +2,7 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
+DROP SCHEMA IF EXISTS `gea-edu` ;
 CREATE SCHEMA IF NOT EXISTS `gea-edu` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci ;
 USE `gea-edu`;
 
@@ -24,35 +25,40 @@ COLLATE = utf8_spanish_ci;
 
 
 -- -----------------------------------------------------
--- Table `gea-edu`.`Horario`
+-- Table `gea-edu`.`Materia`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `gea-edu`.`Horario` (
-  `idHorario` VARCHAR(12) NOT NULL ,
-  `Catedratico_Clave` VARCHAR(30) NOT NULL ,
-  PRIMARY KEY (`idHorario`, `Catedratico_Clave`) ,
-  INDEX `fk_Horario_Catedratico1` (`Catedratico_Clave` ASC) ,
-  CONSTRAINT `fk_Horario_Catedratico1`
-    FOREIGN KEY (`Catedratico_Clave` )
-    REFERENCES `gea-edu`.`Catedratico` (`Clave` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+CREATE  TABLE IF NOT EXISTS `gea-edu`.`Materia` (
+  `Clave` VARCHAR(30) NOT NULL ,
+  `Nombre` VARCHAR(45) NOT NULL ,
+  `Semestre` VARCHAR(45) NOT NULL ,
+  `Creditos` VARCHAR(45) NULL DEFAULT NULL ,
+  PRIMARY KEY (`Clave`) )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
 
 
 -- -----------------------------------------------------
--- Table `gea-edu`.`Grupo`
+-- Table `gea-edu`.`HorarioM`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `gea-edu`.`Grupo` (
-  `idGrupo` VARCHAR(10) NOT NULL ,
-  `Semestre` VARCHAR(45) NOT NULL ,
-  `Horario_idHorario` VARCHAR(12) NOT NULL ,
-  PRIMARY KEY (`idGrupo`, `Horario_idHorario`) ,
-  INDEX `fk_Grupo_Horario1` (`Horario_idHorario` ASC) ,
-  CONSTRAINT `fk_Grupo_Horario1`
-    FOREIGN KEY (`Horario_idHorario` )
-    REFERENCES `gea-edu`.`Horario` (`idHorario` )
+CREATE  TABLE IF NOT EXISTS `gea-edu`.`HorarioM` (
+  `idHorarioM` VARCHAR(15) NOT NULL ,
+  `Dia` DATE NULL ,
+  `Hora` TIME NULL ,
+  `Catedratico_Clave` VARCHAR(30) NOT NULL ,
+  `Materia_Clave` VARCHAR(30) NOT NULL ,
+  `Materia_Horario_idHorario` VARCHAR(12) NOT NULL ,
+  PRIMARY KEY (`idHorarioM`, `Catedratico_Clave`, `Materia_Clave`, `Materia_Horario_idHorario`) ,
+  INDEX `fk_HorarioM_Catedratico1` (`Catedratico_Clave` ASC) ,
+  INDEX `fk_HorarioM_Materia1` (`Materia_Clave` ASC, `Materia_Horario_idHorario` ASC) ,
+  CONSTRAINT `fk_HorarioM_Catedratico1`
+    FOREIGN KEY (`Catedratico_Clave` )
+    REFERENCES `gea-edu`.`Catedratico` (`Clave` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_HorarioM_Materia1`
+    FOREIGN KEY (`Materia_Clave` , `Materia_Horario_idHorario` )
+    REFERENCES `gea-edu`.`Materia` (`Clave` , `Horario_idHorario` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -69,13 +75,25 @@ CREATE  TABLE IF NOT EXISTS `gea-edu`.`Alumno` (
   `Apeido_Materno` VARCHAR(45) NOT NULL ,
   `Telefono` VARCHAR(45) NULL DEFAULT NULL ,
   `Email` VARCHAR(45) NULL DEFAULT NULL ,
-  `Grupo_idGrupo` VARCHAR(10) NOT NULL ,
-  `Grupo_Horario_idHorario` VARCHAR(12) NOT NULL ,
-  PRIMARY KEY (`Numero_de_cuenta`, `Grupo_idGrupo`, `Grupo_Horario_idHorario`) ,
-  INDEX `fk_Alumno_Grupo1` (`Grupo_idGrupo` ASC, `Grupo_Horario_idHorario` ASC) ,
-  CONSTRAINT `fk_Alumno_Grupo1`
-    FOREIGN KEY (`Grupo_idGrupo` , `Grupo_Horario_idHorario` )
-    REFERENCES `gea-edu`.`Grupo` (`idGrupo` , `Horario_idHorario` )
+  PRIMARY KEY (`Numero_de_cuenta`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_spanish_ci;
+
+
+-- -----------------------------------------------------
+-- Table `gea-edu`.`HorarioA`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `gea-edu`.`HorarioA` (
+  `idHorarioA` VARCHAR(15) NOT NULL ,
+  `Dia` DATE NOT NULL ,
+  `Hora` TIME NOT NULL ,
+  `Alumno_Numero_de_cuenta` VARCHAR(10) NOT NULL ,
+  PRIMARY KEY (`idHorarioA`, `Alumno_Numero_de_cuenta`) ,
+  INDEX `fk_HorarioA_Alumno` (`Alumno_Numero_de_cuenta` ASC) ,
+  CONSTRAINT `fk_HorarioA_Alumno`
+    FOREIGN KEY (`Alumno_Numero_de_cuenta` )
+    REFERENCES `gea-edu`.`Alumno` (`Numero_de_cuenta` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -84,19 +102,28 @@ COLLATE = utf8_spanish_ci;
 
 
 -- -----------------------------------------------------
--- Table `gea-edu`.`Materia`
+-- Table `gea-edu`.`Grupo`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `gea-edu`.`Materia` (
-  `Clave` VARCHAR(30) NOT NULL ,
-  `Nombre` VARCHAR(45) NOT NULL ,
+CREATE  TABLE IF NOT EXISTS `gea-edu`.`Grupo` (
+  `idGrupo` VARCHAR(10) NOT NULL ,
   `Semestre` VARCHAR(45) NOT NULL ,
-  `Creditos` VARCHAR(45) NULL DEFAULT NULL ,
-  `Horario_idHorario` VARCHAR(12) NOT NULL ,
-  PRIMARY KEY (`Clave`, `Horario_idHorario`) ,
-  INDEX `fk_Materia_Horario1` (`Horario_idHorario` ASC) ,
-  CONSTRAINT `fk_Materia_Horario1`
-    FOREIGN KEY (`Horario_idHorario` )
-    REFERENCES `gea-edu`.`Horario` (`idHorario` )
+  `HorarioM_idHorarioM` INT NOT NULL ,
+  `HorarioM_Catedratico_Clave` VARCHAR(30) NOT NULL ,
+  `HorarioM_Materia_Clave` VARCHAR(30) NOT NULL ,
+  `HorarioM_Materia_Horario_idHorario` VARCHAR(12) NOT NULL ,
+  `HorarioA_idHorarioA` INT NOT NULL ,
+  `HorarioA_Alumno_Numero_de_cuenta` VARCHAR(10) NOT NULL ,
+  PRIMARY KEY (`idGrupo`, `HorarioM_idHorarioM`, `HorarioM_Catedratico_Clave`, `HorarioM_Materia_Clave`, `HorarioM_Materia_Horario_idHorario`, `HorarioA_idHorarioA`, `HorarioA_Alumno_Numero_de_cuenta`) ,
+  INDEX `fk_Grupo_HorarioM1` (`HorarioM_idHorarioM` ASC, `HorarioM_Catedratico_Clave` ASC, `HorarioM_Materia_Clave` ASC, `HorarioM_Materia_Horario_idHorario` ASC) ,
+  INDEX `fk_Grupo_HorarioA1` (`HorarioA_idHorarioA` ASC, `HorarioA_Alumno_Numero_de_cuenta` ASC) ,
+  CONSTRAINT `fk_Grupo_HorarioM1`
+    FOREIGN KEY (`HorarioM_idHorarioM` , `HorarioM_Catedratico_Clave` , `HorarioM_Materia_Clave` , `HorarioM_Materia_Horario_idHorario` )
+    REFERENCES `gea-edu`.`HorarioM` (`idHorarioM` , `Catedratico_Clave` , `Materia_Clave` , `Materia_Horario_idHorario` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Grupo_HorarioA1`
+    FOREIGN KEY (`HorarioA_idHorarioA` , `HorarioA_Alumno_Numero_de_cuenta` )
+    REFERENCES `gea-edu`.`HorarioA` (`idHorarioA` , `Alumno_Numero_de_cuenta` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
