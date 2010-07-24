b948bec4cb671df8530c1db73f5a20d4cb3b2b3f
thelordofdragon@gmail.com <thelordofdragon@gmail.com@8f31eaa2-f0ef-11de-b2c3-d77de7315633>
2010-07-24 19:47:53 +0000

    class changes to the form1 and form2

--- trunk/WebContent/cui/form1.jsp
+++ trunk/WebContent/cui/form1.jsp
@@ -1,8 +1,7 @@
 <?xml version="1.0" encoding="UTF-8" ?>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@ include file="conexion.jsp"%>
-<%@page import="java.util.regex.*"%>      
+<%@page import="core.connector" %>         
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -12,9 +11,6 @@
 <body>
 <%
 try{
-	conexion=(Connection)session.getAttribute("conexion");
-	sentenciaSQL = conexion.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
-		ResultSet.CONCUR_UPDATABLE);
 		String ncuenta=request.getParameter("n_cuenta");
 		String nombre=request.getParameter("nombre");
 		String apaterno=request.getParameter("A_paterno");
@@ -34,25 +30,9 @@ try{
 		String codigop=request.getParameter("cp");
 		String Genero="otro";
 		
-		/*if(genero1.equals("femenino")==true){
-		  Genero="femenino";
-		}
-		else{
-			if(genero2.equals("masculino")==true){
-				Genero="masculino";		
-			}
-			else{
-				Genero="otro";
-			}
-		}*/
-		
-		
-        sentenciaSQL.executeUpdate("INSERT INTO Tabla_Alumno VALUES('"+ncuenta+"', '"+nombre+"','"+apaterno+"','"+amaterno+"','"+edad+"','"+telefono+"','"+correo+"','"+Genero+"','"+calle+"','"+colonia+"','"+municipio+"','"+estado+"','"+codigop+"');");
-        out.println("Se a registrado al alumno");
-        conexion.close();
+		connector.formu1(ncuenta, nombre , apaterno ,  amaterno ,  edad , telefono , correo ,  Genero ,  calle,  colonia , municipio , estado , codigop );
     }catch(Exception e){
 			 out.println("error: "+e+"<br/>");
-			 
 		
 		}
     
--- trunk/src/core/connector.java
+++ trunk/src/core/connector.java
@@ -33,13 +33,17 @@ public class connector {
 		Connection conexion = connector.getSqlServerConnection("entrada","entrada"); 
 		Statement   sentenciaSQL;
 		ResultSet   cdr;
-		sentenciaSQL = conexion.createStatement();
-	        cdr=sentenciaSQL.executeQuery("SELECT ncuenta FROM Tabla_Alumno");
+		sentenciaSQL = conexion.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+				ResultSet.CONCUR_UPDATABLE);
+	        cdr=sentenciaSQL.executeQuery("SELECT ncuenta FROM Tabla_Alumno where ncuenta='"+cuenta+"';");
 	        while(cdr.next()){
-	        	if(cdr.getString("ncuenta").equals(cuenta)==true){
+	        	System.out .print(cdr);
+	        	if(cdr.equals(cuenta)==true){
+	            conexion.close();	
 	        	return true;
 	        	}
 	        	else{
+	        		conexion.close();
 	        		return false;
 	        	}
 	        }
@@ -47,4 +51,23 @@ public class connector {
 	        conexion.close();
 		    return false;
 	}
+	
+	 public static void formu1(String ncuenta, String nombre , String apaterno , String amaterno , String edad , String telefono ,String correo , String Genero , String calle, String colonia , String municipio , String estado , String codigop ) throws SQLException{
+		 Connection conexion = connector.getSqlServerConnection("entrada","entrada"); 
+			Statement   sentenciaSQL;
+			sentenciaSQL = conexion.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_UPDATABLE);
+			sentenciaSQL.executeUpdate("INSERT INTO Tabla_Alumno VALUES('"+ncuenta+"', '"+nombre+"','"+apaterno+"','"+amaterno+"','"+edad+"','"+telefono+"','"+correo+"','"+Genero+"','"+calle+"','"+colonia+"','"+municipio+"','"+estado+"','"+codigop+"');");	
+			conexion.close();
+	 }
+	 public static void formu2(String clave, String nombre, String curso, String semestre) throws SQLException{
+		 Connection conexion = connector.getSqlServerConnection("entrada","entrada");
+		 Statement   sentenciaSQL;
+		 sentenciaSQL = conexion.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_UPDATABLE);
+		sentenciaSQL.executeUpdate("INSERT INTO Tabla_Alumno VALUES('"+clave+"', '"+nombre+"','"+curso+"','"+semestre+"');");
+		conexion.close();
+	 }
+
+	 
 }
