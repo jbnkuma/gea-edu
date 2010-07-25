72c49fa707ec8720ab92840cf06fdd89bab56fbd
thelordofdragon@gmail.com <thelordofdragon@gmail.com@8f31eaa2-f0ef-11de-b2c3-d77de7315633>
2010-07-25 23:48:14 +0000

    añadiendo plugin jquery, eliminando archivos que no tienen uso

--- trunk/WebContent/cui/scripts/menu.js
+++ trunk/WebContent/cui/scripts/menu.js
@@ -1,12 +1,25 @@
-<script type="text/javascript" charset="utf-8">
+ var scrl = " GEA-CUI ";
+   function scrlsts() {
+   scrl = scrl.substring(1, scrl.length) + scrl.substring(0, 1);
+   document.title = scrl;
+   setTimeout("scrlsts()", 300);
+   }
+   
 $(function(){
-	$('#menu li a').click(function(event){
-		var elem = $(this).next();
-		if(elem.is('ul')){
-			event.preventDefault();
-			$('#menu ul:visible').not(elem).slideUp();
-			elem.slideToggle();
-		}
-	});
+		$('#menu1 li a').click(function(event){
+			var elem = $(this).next();
+			if(elem.is('ul')){
+				event.preventDefault();
+				$('#menu1 ul:visible').not(elem).slideUp();
+				elem.slideToggle();
+			}
+		});
+	});  
+
+$(function() {
+    //Todos los formularios con clase jqtransform
+
+    $("form.jqtransform").jqTransform();
+
 });
-</script>
+
